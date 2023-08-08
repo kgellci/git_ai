f3de488de88bf0893d881574b90c075158798bf1
Kris Gellci <kgellci@gmail.com>
2023-08-08 13:50:11 -0700

    Update prompt in ai_commit.sh to be more descriptive

--- ai_commit.sh
+++ ai_commit.sh
@@ -39,7 +39,7 @@
         escaped_diff=$(escape_to_json "$diff")
 
         if [ -z "$OVERRIDE_PROMPT" ]; then
-            local prompt="Write a short and concise commit message."
+            local prompt="Write a short and descriptive commit message."
         else
             local prompt=$OVERRIDE_PROMPT
         fi
@@ -60,11 +60,20 @@
         }'
         
         # Call GPT-3.5 to generate a commit message
-        commit_message=$(curl -s -X POST "https://api.openai.com/v1/chat/completions" \
+        response=$(curl -s -X POST "https://api.openai.com/v1/chat/completions" \
                             -H "Content-Type: application/json" \
                             -H "Authorization: Bearer $API_KEY" \
-                            -d "$request_body" | grep -o '<ai_commit_response>[^<]*</ai_commit_response>' | sed 's/<ai_commit_response>//; s/<\/ai_commit_response>//')
-        
+                            -d "$request_body")
+
+        # Check if there was an error with the curl request
+        if [ $? -ne 0 ]; then
+            echo "Error occurred while making the API request."
+            exit 1
+        fi
+
+        # Extract commit message from response
+        commit_message=$(echo "$response" | grep -o '<ai_commit_response>[^<]*</ai_commit_response>' | sed 's/<ai_commit_response>//; s/<\/ai_commit_response>//')
+
         echo "$commit_message"
     }
 
