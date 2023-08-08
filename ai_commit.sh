e1a4de4fe73c3768643315842ca0d8763e7e36e6
Kris Gellci <kgellci@gmail.com>
2023-08-08 10:50:01 -0700

    Add ai_commit.sh and .gitignore

---
+++ ai_commit.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+(
+       # Check if config file exists in the same directory as the script
+    CONFIG_FILE="$(dirname "$0")/config.txt"
+    if [ ! -f "$CONFIG_FILE" ]; then
+        echo "Config file 'config.txt' not found in the same directory as the script."
+        exit 1
+    fi
+
+    # Source the config file to get the API key
+    source "$CONFIG_FILE"
+
+    # Define the Git diff command
+    DIFF_CMD="git diff --cached"
+
+    # Add changes to the staging area
+    git add .
+
+    # Execute the Git diff command and capture its output
+    DIFF_OUTPUT="$($DIFF_CMD)"
+
+    # Function to escape and format a string as JSON
+    escape_to_json() {
+        local string="$1"
+        # Escape special characters: \, ", and /
+        string="${string//\\/\\\\}"
+        string="${string//\"/\\\"}"
+        string="${string//\//\\/}"
+        # Replace newlines with \n and tabs with \t
+        string="${string//$'\n'/\\n}"
+        string="${string//$'\t'/\\t}"
+        echo "$string"
+    }
+
+    # Function to interact with GPT-3.5 and generate a commit message
+    generate_commit_message() {
+        local diff="$1"
+        escaped_diff=$(escape_to_json "$diff")
+        local prompt="Write a short and concise commit message for the following diff:\n\n'$escaped_diff'"
+
+        # Construct the JSON request body
+        request_body='{
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "'"$prompt"'"
+                }
+            ]
+        }'
+        
+        # Call GPT-3.5 to generate a commit message
+        commit_message=$(curl -s -X POST "https://api.openai.com/v1/chat/completions" \
+                            -H "Content-Type: application/json" \
+                            -H "Authorization: Bearer $API_KEY" \
+                            -d "$request_body" | grep -o '"content": "[^"]*' | sed 's/"content": "//')
+        
+        echo "$commit_message"
+    }
+
+    # Generate a commit message using GPT-3.5
+    commit_msg=$(generate_commit_message "$DIFF_OUTPUT")
+
+    echo "$commit_msg"
+    read -p "Do you want to commit these changes with the above message? (y/n): " user_choice
+
+    if [[ $user_choice == "y" || $user_choice == "Y" ]]; then
+        git commit -m "$commit_msg"
+        echo "Changes committed with the generated message."
+    else
+        echo "Changes not committed."
+    fi
+)
